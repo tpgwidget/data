68170d570c321a15d63d6171889f9f266626568e
Nicolapps <webmaster@nicolapps.ch>
2018-03-16 08:34:55 +0000

    Add stop corrections

--- src/Stops.php
+++ src/Stops.php
@@ -2,8 +2,62 @@
 namespace TPGwidget\Data;
 
 class Stops {
+    const STOP_NAMES_CORRECTIONS = [
+        'Gare Eaux-Vives' => 'Gare des Eaux-Vives',
+        'Ziplo' => 'ZIPLO',
+        'De stael' => 'De-Staël <small>(dépôt)</small>',
+        'Palettes - armes' => 'Palettes - Armes',
+        'Jar.-Botanique' => 'Jardin Botanique',
+        'Bachet' => 'Bachet-de-Pesay <small>(dépôt)</small>',
+        'Palettes bachet' => 'Palettes - Bachet',
+        'Onex-cite' => 'Onex-Cité',
+        'Lignon' => 'Lignon-Tours',
+        'Carouge' => 'Carouge-Rondeau',
+        'Carouge (Rondeau)' => 'Carouge-Rondeau',
+        'Thonex-vallard' => 'Thônex-Vallard',
+        'Th-Vallard-Dne' => 'Thônex-Vallard',
+        'Place de neuve' => 'Place de Neuve',
+        'Champel' => 'Crêts-de-Champel',
+        'Neydens-vitam' => 'Neydens-Vitam',
+        'Vernier' => 'Vernier-Village',
+        'Champel' => 'Crêts-de-Champel',
+        'P+R Etoile' => 'P+R Étoile',
+        'Oms' => 'OMS',
+        'P+r veigy' => 'P+R Veigy',
+        'P bernex' => 'P+R Bernex',
+        'Puplinge-mairie' => 'Puplinge-Mairie',
+        'Veyrier-Tournet.' => 'Veyrier-Tournettes',
+        'Palettes-Bachet' => 'Palettes - Bachet <small>(dépôt)</small>',
+        'Cs la becassiere' => 'CS La Bécassière',
+        'Annemasse-gare' => 'Annemasse-Gare',
+        'Annemasse gare' => 'Annemasse-G. EXPRESS',
+        'Ecole medecine' => 'École-de-Médecine',
+        'Hopital la tour' => 'Hôpital de la Tour',
+        'Loëx-hôpital' => 'Loëx-Hôpital',
+        'C.o Renard' => 'CO Renard',
+        'Co renard' => 'CO Renard',
+        'Challex-la halle' => 'P+R Challex-La Halle',
+        'Sezenove' => 'Sézenove',
+        'Emile zola' => 'Émile Zola',
+        'Hopitaux' => 'Hôpitaux',
+        'Lancy-hubert' => 'Lancy-Hubert',
+        'Lancy - hubert' => 'Lancy-Hubert',
+        'Coll.Claparède' => 'Collège Claparède',
+        'Pl. Eaux-Vives' => 'Place des Eaux-Vives',
+        'La plaine-gare' => 'La Plaine-Gare',
+        'Gd-Saconnex-Place' => 'Grand-Saconnex-Place',
+        'Gd-Saconnex-Douane' => 'Grand-Saconnex-Douane',
+        'Gd-Saconnex-Mairie' => 'Grand-Saconnex-Mairie',
+        'Hôpital la Tour' => 'Hôpital de La Tour',
+        'Jardin-Alpin-Vi.' => 'Jardin-Alpin-Vivarium',
+        'Aeroport-p47' => 'Aéroport-P47',
+        'Vernier-Parf.' => 'Vernier-Parfumerie',
+        'Les esserts' => 'Les Esserts',
+        'Jardin botanique' => 'Jardin Botanique',
+    ];
+    
     public static function correct(string $stopName): string
     {
-        return $stopName;
+        return self::STOP_NAMES_CORRECTIONS[$stopName] ?? $stopName;
     }
 }
--- tests/StopsTest.php
+++ tests/StopsTest.php
@@ -11,4 +11,14 @@ class StopsTest extends TestCase
         $output = Stops::correct('xyz');
         $this->assertEquals('xyz', $output);
     }
+    
+    /** @test */
+    public function it_should_correct_wrong_stop_names()
+    {
+        $output = Stops::correct('Gd-Saconnex-Place');
+        $this->assertEquals('Grand-Saconnex-Place', $output);
+        
+        $output = Stops::correct('Vernier-Parf.');
+        $this->assertEquals('Vernier-Parfumerie', $output);
+    }
 }
