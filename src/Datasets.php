34676287b9ddc4263c52040ac2d094ad77fb3761
Nicolapps <webmaster@nicolapps.ch>
2018-03-23 09:32:13 +0000

    Use the stopNames.json dataset

--- src/Datasets.php
+++ src/Datasets.php
@@ -4,19 +4,35 @@ namespace TPGwidget\Data;
 class Datasets {
     private static $datasets = [];
     
+    /**
+     * Loads a dataset file
+     * 
+     * @param string $datasetName The name of the file that should be loaded
+     * 
+     * @throws \InvalidArgumentException if the file couldn't be loaded (e.g. it doesn't exist)
+     * @throws \DomainException if the file couldn't be parsed (e.g. invalid JSON content)
+     * 
+     * @return array The JSON-decoded dataset (associative array form)
+     */
     public static function load(string $datasetName): array
     {
-        if (array_key_exists($datasetName, self::$datasets)) { // Already loaded
+        // Use the datasets cache
+        if (array_key_exists($datasetName, self::$datasets)) {
             return self::$datasets[$datasetName];
         }
         
+        // Load the file
         $file = @file_get_contents(__DIR__.'/../data/'.$datasetName.'.json');
-        
         if ($file === false) {
             throw new \InvalidArgumentException('Dataset file `'.$datasetName.'.json` couldn’t be loaded.');
         }
         
+        // Parse the JSON content
         $content = json_decode($file, true);
+        if (is_null($content)) {
+            throw new \DomainException('Dataset file `'.$datasetName.'.json` contains invalid data.');
+        }
+        
         self::$datasets[$datasetName] = $content;
         return $content;
     }
--- src/Stops.php
+++ src/Stops.php
@@ -2,63 +2,16 @@
 namespace TPGwidget\Data;
 
 class Stops {
-    const STOP_NAMES_CORRECTIONS = [
-        'Gare Eaux-Vives' => 'Gare des Eaux-Vives',
-        'Ziplo' => 'ZIPLO',
-        'De stael' => 'De-Staël',
-        'Palettes - armes' => 'Palettes - Armes',
-        'Jar.-Botanique' => 'Jardin Botanique',
-        'Bachet' => 'Bachet-de-Pesay',
-        'Palettes bachet' => 'Palettes - Bachet',
-        'Onex-cite' => 'Onex-Cité',
-        'Lignon' => 'Lignon-Tours',
-        'Carouge' => 'Carouge-Rondeau',
-        'Carouge (Rondeau)' => 'Carouge-Rondeau',
-        'Thonex-vallard' => 'Thônex-Vallard',
-        'Th-Vallard-Dne' => 'Thônex-Vallard',
-        'Place de neuve' => 'Place de Neuve',
-        'Champel' => 'Crêts-de-Champel',
-        'Neydens-vitam' => 'Neydens-Vitam',
-        'Vernier' => 'Vernier-Village',
-        'Champel' => 'Crêts-de-Champel',
-        'P+R Etoile' => 'P+R Étoile',
-        'Oms' => 'OMS',
-        'P+r veigy' => 'P+R Veigy',
-        'P bernex' => 'P+R Bernex',
-        'Puplinge-mairie' => 'Puplinge-Mairie',
-        'Veyrier-Tournet.' => 'Veyrier-Tournettes',
-        'Palettes-Bachet' => 'Palettes - Bachet',
-        'Cs la becassiere' => 'CS La Bécassière',
-        'Annemasse-gare' => 'Annemasse-Gare',
-        'Annemasse gare' => 'Annemasse-G. EXPRESS',
-        'Ecole medecine' => 'École-de-Médecine',
-        'Hopital la tour' => 'Hôpital de la Tour',
-        'Loëx-hôpital' => 'Loëx-Hôpital',
-        'C.o Renard' => 'CO Renard',
-        'Co renard' => 'CO Renard',
-        'Challex-la halle' => 'P+R Challex-La Halle',
-        'Sezenove' => 'Sézenove',
-        'Emile zola' => 'Émile Zola',
-        'Hopitaux' => 'Hôpitaux',
-        'Lancy-hubert' => 'Lancy-Hubert',
-        'Lancy - hubert' => 'Lancy-Hubert',
-        'Coll.Claparède' => 'Collège Claparède',
-        'Pl. Eaux-Vives' => 'Place des Eaux-Vives',
-        'La plaine-gare' => 'La Plaine-Gare',
-        'Gd-Saconnex-Place' => 'Grand-Saconnex-Place',
-        'Gd-Saconnex-Douane' => 'Grand-Saconnex-Douane',
-        'Gd-Saconnex-Mairie' => 'Grand-Saconnex-Mairie',
-        'Hôpital la Tour' => 'Hôpital de La Tour',
-        'Jardin-Alpin-Vi.' => 'Jardin-Alpin-Vivarium',
-        'Aeroport-p47' => 'Aéroport-P47',
-        'Vernier-Parf.' => 'Vernier-Parfumerie',
-        'Les esserts' => 'Les Esserts',
-        'Jardin botanique' => 'Jardin Botanique',
-        'Ste-Clotilde' => 'Sainte-Clotilde',
-    ];
-    
+    /**
+     * Corrects a stop name
+     * 
+     * @param string $stopName The stop name returned by the TPG API
+     * 
+     * @return string The corrected stop name
+     *                (or the original $stopName parameter if no correction exists)
+     */
     public static function correct(string $stopName): string
     {
-        return self::STOP_NAMES_CORRECTIONS[$stopName] ?? $stopName;
+        return Datasets::load('stopNames')[$stopName] ?? $stopName;
     }
 }
